a2048ed59f8b56551ae420860bab91c464ca3174
Brian Zavala <brianzav23@gmail.com>
2024-03-03 19:25:37 -0300

    First commit

---
+++ src/main/java/br/com/senac/gamerx/GamerxApplication.java
@@ -0,0 +1,13 @@
+package br.com.senac.gamerx;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GamerxApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GamerxApplication.class, args);
+	}
+
+}
