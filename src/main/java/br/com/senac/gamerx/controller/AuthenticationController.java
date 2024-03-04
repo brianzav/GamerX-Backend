364e02b5817aa86a157ce87b8f615eac373e8c12
Brian Zavala <brianzav23@gmail.com>
2024-03-04 00:07:38 -0300

    adding new dto

--- src/main/java/br/com/senac/gamerx/controller/AuthenticationController.java
+++ src/main/java/br/com/senac/gamerx/controller/AuthenticationController.java
@@ -1,11 +1,15 @@
 package br.com.senac.gamerx.controller;
 
 import br.com.senac.gamerx.dto.AuthenticationDTO;
+import br.com.senac.gamerx.dto.RegisterDTO;
+import br.com.senac.gamerx.model.UserModel;
+import br.com.senac.gamerx.repository.UserRepository;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -17,11 +21,21 @@ import org.springframework.web.bind.annotation.RestController;
 public class AuthenticationController {
     @Autowired
     private AuthenticationManager authenticationManager;
+    @Autowired
+    private UserRepository repository;
     @PostMapping("/login")
     public ResponseEntity login(@RequestBody @Valid AuthenticationDTO data){
         var usernamePassword = new UsernamePasswordAuthenticationToken(data.email(), data.password());
         var auth = this.authenticationManager.authenticate(usernamePassword);
 
         return ResponseEntity.ok().build();
+    }
+
+    //TODO CRIAR CONSTRUTOR DO USER MODEL COM TODOS OS ATRIBUTOS
+    @PostMapping("/register")
+    public ResponseEntity register(@RequestBody @Valid RegisterDTO data){
+        if(this.repository.findByEmail(data.email()) != null) return ResponseEntity.badRequest().build();
+        String encryptedPassword = new BCryptPasswordEncoder().encode(data.password());
+        UserModel newUser = new UserModel(data.email(), encryptedPassword, data.role(), da);
     }
 }
---
+++ src/main/java/br/com/senac/gamerx/dto/RegisterDTO.java
@@ -0,0 +1,4 @@
+package br.com.senac.gamerx.dto;
+
+public record RegisterDTO(String email, String password, String role) {
+}
