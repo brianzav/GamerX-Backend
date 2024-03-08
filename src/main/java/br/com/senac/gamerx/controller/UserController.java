56ff9eb03981bdee4ae8eab6e2fc3c03e5935021
Brian Zavala <brianzav23@gmail.com>
2024-03-08 17:49:03 -0300

    adding new endpoints for user

--- src/main/java/br/com/senac/gamerx/controller/UserController.java
+++ src/main/java/br/com/senac/gamerx/controller/UserController.java
@@ -1,9 +1,36 @@
 package br.com.senac.gamerx.controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import br.com.senac.gamerx.model.UserModel;
+import br.com.senac.gamerx.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
 
-@RequestMapping("users")
+import java.util.List;
+
 @RestController
+@RequestMapping("/users") //
 public class UserController {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @GetMapping
+    public List<UserModel> listUsers() {
+        return userRepository.findAll();
+    }
+
+    @PatchMapping("/toggle-status/{userId}")
+    public ResponseEntity<?> toggleUserStatus(@PathVariable Long userId) {
+        UserModel user = userRepository.findById(String.valueOf(userId))
+                .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Usuário não encontrado"));
+
+        user.setActive(!user.isActive()); // Inverte o status do usuário
+        userRepository.save(user);
+
+        return ResponseEntity.ok().build(); // Retorna uma resposta de sucesso
+    }
 }
+
